4cbc7e2901490fb855169cf61f79f55fddf221a2
Furkan Tufekci <furkan._t@hotmail.com>
2012-05-15 21:12:59 -0400

    Rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TelaKKi::Application.config.secret_token = '9510e50e5cbb86b45267a06c4fb05ec9806c078377f2b0cc62ff15e7b96734cab2c9830044deee8e7a7fb0132ad446f993bfbd8891fe2cb3f52bc27b6471d561'
