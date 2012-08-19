44c1d6a8f4a01daaa67529858dde97dc3b6e86cb
Furkan Tufekci <furkan._t@hotmail.com>
2012-08-19 19:17:46 -0400

    Forgot to close vim before check-in -_-

--- config/initializers/mailboxer.rb
+++ config/initializers/mailboxer.rb
@@ -1,7 +1,7 @@
 Mailboxer.setup do |config|
   
   #Configures if you applications uses or no the email sending for Notifications and Messages
-  config.uses_emails = true
+  config.uses_emails = false 
   
   #Configures the default from for the email sent for Messages and Notifications of Mailboxer
   config.default_from = "no-reply@mailboxer.com"
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120805214142) do
+ActiveRecord::Schema.define(:version => 20120819230845) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -65,6 +65,12 @@ ActiveRecord::Schema.define(:version => 20120805214142) do
     t.datetime "updated_at", :null => false
   end
 
+  create_table "conversations", :force => true do |t|
+    t.string   "subject",    :default => ""
+    t.datetime "created_at",                 :null => false
+    t.datetime "updated_at",                 :null => false
+  end
+
   create_table "images", :force => true do |t|
     t.datetime "created_at",         :null => false
     t.datetime "updated_at",         :null => false
@@ -107,11 +113,43 @@ ActiveRecord::Schema.define(:version => 20120805214142) do
     t.datetime "updated_at",                    :null => false
   end
 
+  create_table "notifications", :force => true do |t|
+    t.string   "type"
+    t.text     "body"
+    t.string   "subject",              :default => ""
+    t.integer  "sender_id"
+    t.string   "sender_type"
+    t.integer  "conversation_id"
+    t.boolean  "draft",                :default => false
+    t.datetime "updated_at",                              :null => false
+    t.datetime "created_at",                              :null => false
+    t.integer  "notified_object_id"
+    t.string   "notified_object_type"
+    t.string   "notification_code"
+    t.string   "attachment"
+  end
+
+  add_index "notifications", ["conversation_id"], :name => "index_notifications_on_conversation_id"
+
   create_table "prelaunches", :force => true do |t|
     t.string   "email"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
+
+  create_table "receipts", :force => true do |t|
+    t.integer  "receiver_id"
+    t.string   "receiver_type"
+    t.integer  "notification_id",                                  :null => false
+    t.boolean  "is_read",                       :default => false
+    t.boolean  "trashed",                       :default => false
+    t.boolean  "deleted",                       :default => false
+    t.string   "mailbox_type",    :limit => 25
+    t.datetime "created_at",                                       :null => false
+    t.datetime "updated_at",                                       :null => false
+  end
+
+  add_index "receipts", ["notification_id"], :name => "index_receipts_on_notification_id"
 
   create_table "settings", :force => true do |t|
     t.string   "city"
