2cc7d03a780cb7512883165402b935d4ad63a303
Furkan Tufekci <furkan._t@hotmail.com>
2012-09-30 20:15:29 -0400

    Basic categories finished!

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -10,6 +10,7 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find(params[:id])
+    @items = Item.where(category_id: @category.id).all
 
     respond_to do |format|
       format.html # show.html.erb
