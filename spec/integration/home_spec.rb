dc6a16d6ec25ec047dfc54fb187e1ba6321a9087
ftufek <furkan._t@hotmail.com>
2012-11-06 13:40:02 -0500

    Spork bootstrapped

--- spec/integration/home_spec.rb
+++ spec/integration/home_spec.rb
@@ -1,6 +1,6 @@
-describe 'home page' do
-  it 'contains the navbar' do
-    visit '/'
-    page.should have_selector('inexistent')
-  end
-end
+#describe 'home page' do
+#  it 'contains the navbar' do
+#    visit '/'
+#    page.should have_selector('inexistent')
+#  end
+#end
